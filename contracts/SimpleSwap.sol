287f8c8ed49f963a7d5408edaca6e27f4e2d8246
white <t100210022002@gmail.com>
2022-12-24 15:02:12 +0800

    change the function order

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -16,46 +16,76 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
     /* ------------------------------------------------------ */
     /*                        DATA SETS                       */
     /* ------------------------------------------------------ */
-    address public s_tokenA;
-    address public s_tokenB;
-    uint256 private s_reserveA;
-    uint256 private s_reserveB;
+    address public tokenA_;
+    address public tokenB_;
+    uint256 private reserveA_;
+    uint256 private reserveB_;
     /* ------------------------------------------------------ */
     /*                        MODIFIERS                       */
     /* ------------------------------------------------------ */
-    uint256 private s_unlocked = 1;
+    uint256 private unlocked_ = 1;
     modifier lock() {
-        require(s_unlocked == 1, "SimpleSwap: LOCKED");
-        s_unlocked = 0;
+        require(unlocked_ == 1, "SimpleSwap: LOCKED");
+        unlocked_ = 0;
         _;
-        s_unlocked = 1;
+        unlocked_ = 1;
     }
 
     constructor(address _tokenA, address _tokenB) public ERC20("SimpleSwap", "SPI") {
         require(_tokenA != address(0), "SimpleSwap: TOKENA_IS_NOT_CONTRACT");
         require(_tokenB != address(0), "SimpleSwap: TOKENB_IS_NOT_CONTRACT");
         require(_tokenA != _tokenB, "SimpleSwap: TOKENA_TOKENB_IDENTICAL_ADDRESS");
-        s_tokenA = _tokenA;
-        s_tokenB = _tokenB;
+        tokenA_ = _tokenA;
+        tokenB_ = _tokenB;
+    }
+
+    /* ------------------------------------------------------ */
+    /*                      Math library                      */
+    /* ------------------------------------------------------ */
+    function min(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = x < y ? x : y;
+    }
+
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
     }
 
     /* ------------------------------------------------------ */
     /*                     PUBLIC FUNCTION                    */
     /* ------------------------------------------------------ */
+
     function getReserves() public view override returns (uint256 reserveA, uint256 reserveB) {
-        return (s_reserveA, s_reserveB);
+        return (reserveA_, reserveB_);
     }
 
     /* ------------------------------------------------------ */
     /*                    EXTERNAL FUNCTION                   */
     /* ------------------------------------------------------ */
+
+    function getTokenA() external view override returns (address tokenA) {
+        return tokenA_;
+    }
+
+    function getTokenB() external view override returns (address tokenB) {
+        return tokenB_;
+    }
+
     function swap(
         address tokenIn,
         address tokenOut,
         uint256 amountIn
     ) external override lock returns (uint256 amountOut) {
-        address _tokenA = s_tokenA;
-        address _tokenB = s_tokenB;
+        address _tokenA = tokenA_;
+        address _tokenB = tokenB_;
 
         require(amountIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
         require(tokenIn == _tokenA || tokenIn == _tokenB, "SimpleSwap: INVALID_TOKEN_IN");
@@ -64,8 +94,8 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         require(msg.sender != _tokenA && msg.sender != _tokenB, "SimpleSwap: INVALID_TO");
 
         (uint256 reserveInput, uint256 reserveOutput) = tokenIn == _tokenA
-            ? (s_reserveA, s_reserveB)
-            : (s_reserveB, s_reserveA);
+            ? (reserveA_, reserveB_)
+            : (reserveB_, reserveA_);
         require(reserveInput > 0 && reserveOutput > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY");
 
         ERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
@@ -95,7 +125,7 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
             ? (balanceAForK = balanceAForK - amountIn * FEEPERSENT_NUMERATOR)
             : (balanceBForK = balanceBForK - amountIn * FEEPERSENT_NUMERATOR);
         require(
-            (balanceAForK * balanceBForK) >= reserveInput * reserveOutput * FLOATPADDING**2,
+            (balanceAForK * balanceBForK) >= reserveInput * reserveOutput * FLOATPADDING ** 2,
             "SimpleSwap: INVALID K"
         );
 
@@ -106,29 +136,23 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         return amountOutput;
     }
 
-    function addLiquidity(uint256 amountAIn, uint256 amountBIn)
-        external
-        override
-        lock
-        returns (
-            uint256 amountA,
-            uint256 amountB,
-            uint256 liquidity
-        )
-    {
+    function addLiquidity(
+        uint256 amountAIn,
+        uint256 amountBIn
+    ) external override lock returns (uint256 amountA, uint256 amountB, uint256 liquidity) {
         require(amountAIn > 0 && amountBIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
 
         (uint256 actualAmount, uint256 actualBmount) = _getActualAmount(amountAIn, amountBIn);
 
         (uint256 amountA, uint256 amountB, uint256 liquidity) = mint(actualAmount, actualBmount);
 
-        ERC20(s_tokenA).transferFrom(msg.sender, address(this), actualAmount);
-        ERC20(s_tokenB).transferFrom(msg.sender, address(this), actualBmount);
+        ERC20(tokenA_).transferFrom(msg.sender, address(this), actualAmount);
+        ERC20(tokenB_).transferFrom(msg.sender, address(this), actualBmount);
     }
 
     function removeLiquidity(uint256 liquidity) external override lock returns (uint256 amountA, uint256 amountB) {
-        address _tokenA = s_tokenA;
-        address _tokenB = s_tokenB;
+        address _tokenA = tokenA_;
+        address _tokenB = tokenB_;
 
         uint256 balanceA = ERC20(_tokenA).balanceOf(address(this));
         uint256 balanceB = ERC20(_tokenB).balanceOf(address(this));
@@ -149,46 +173,9 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         return (amountA, amountB);
     }
 
-    function getTokenA() external view override returns (address tokenA) {
-        return s_tokenA;
-    }
-
-    function getTokenB() external view override returns (address tokenB) {
-        return s_tokenB;
-    }
-
     /* ------------------------------------------------------ */
     /*                    INTERNAL FUNCTION                   */
     /* ------------------------------------------------------ */
-    function mint(uint256 amountAIn, uint256 amountBIn)
-        internal
-        returns (
-            uint256 amountA,
-            uint256 amountB,
-            uint256 liquidity
-        )
-    {
-        uint256 amountA = amountAIn;
-        uint256 amountB = amountBIn;
-
-        uint256 _totalSupply = totalSupply();
-        if (_totalSupply == 0) {
-            liquidity = sqrt(amountA * amountB) - MINIMUM_LIQUIDITY;
-            _mint(address(this), MINIMUM_LIQUIDITY);
-        } else {
-            liquidity = min((amountA * _totalSupply) / s_reserveA, (amountB * _totalSupply) / s_reserveB);
-        }
-
-        require(liquidity > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_MINTED");
-
-        _mint(msg.sender, liquidity);
-
-        uint256 newReserveA = ERC20(s_tokenA).balanceOf(address(this)) + amountAIn;
-        uint256 newReserveB = ERC20(s_tokenB).balanceOf(address(this)) + amountBIn;
-        _updateReserves(newReserveA, newReserveB);
-
-        emit AddLiquidity(msg.sender, amountA, amountB, liquidity);
-    }
 
     function _getAmountOut(
         uint256 amountIn,
@@ -212,46 +199,56 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         return reserveOutput - newReserveOutput;
     }
 
-    function _getActualAmount(uint256 amountAIn, uint256 amountBIn)
-        internal
-        returns (uint256 actualAmountOfA, uint256 actualAmountOfB)
-    {
-        if (s_reserveA == 0 && s_reserveB == 0) return (amountAIn, amountBIn);
+    function _getActualAmount(
+        uint256 amountAIn,
+        uint256 amountBIn
+    ) internal view returns (uint256 actualAmountOfA, uint256 actualAmountOfB) {
+        if (reserveA_ == 0 && reserveB_ == 0) return (amountAIn, amountBIn);
         uint256 actualAmountOfA = amountAIn;
         uint256 actualAmountOfB = amountBIn;
-        if (s_reserveA > 0) {
-            uint256 porpotionA = (amountAIn * s_reserveB) / (s_reserveA);
+        if (reserveA_ > 0) {
+            uint256 porpotionA = (amountAIn * reserveB_) / (reserveA_);
             if (amountBIn > porpotionA) actualAmountOfB = porpotionA;
         }
-        if (s_reserveB > 0) {
-            uint256 porpotionB = (amountBIn * (s_reserveA)) / (s_reserveB);
+        if (reserveB_ > 0) {
+            uint256 porpotionB = (amountBIn * (reserveA_)) / (reserveB_);
             if (amountAIn > porpotionB) actualAmountOfA = porpotionB;
         }
         return (actualAmountOfA, actualAmountOfB);
     }
 
-    function _updateReserves(uint256 balanceA, uint256 balanceB) private {
-        s_reserveA = balanceA;
-        s_reserveB = balanceB;
-    }
+    function mint(
+        uint256 amountAIn,
+        uint256 amountBIn
+    ) internal returns (uint256 amountA, uint256 amountB, uint256 liquidity) {
+        uint256 amountA = amountAIn;
+        uint256 amountB = amountBIn;
 
-    /* ------------------------------------------------------ */
-    /*                      Math library                      */
-    /* ------------------------------------------------------ */
-    function min(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = x < y ? x : y;
-    }
-
-    function sqrt(uint256 y) internal pure returns (uint256 z) {
-        if (y > 3) {
-            z = y;
-            uint256 x = y / 2 + 1;
-            while (x < z) {
-                z = x;
-                x = (y / x + x) / 2;
-            }
-        } else if (y != 0) {
-            z = 1;
+        uint256 _totalSupply = totalSupply();
+        if (_totalSupply == 0) {
+            liquidity = sqrt(amountA * amountB) - MINIMUM_LIQUIDITY;
+            _mint(address(this), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = min((amountA * _totalSupply) / reserveA_, (amountB * _totalSupply) / reserveB_);
         }
+
+        require(liquidity > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_MINTED");
+
+        _mint(msg.sender, liquidity);
+
+        uint256 newReserveA = ERC20(tokenA_).balanceOf(address(this)) + amountAIn;
+        uint256 newReserveB = ERC20(tokenB_).balanceOf(address(this)) + amountBIn;
+        _updateReserves(newReserveA, newReserveB);
+
+        emit AddLiquidity(msg.sender, amountA, amountB, liquidity);
+    }
+
+    /* ------------------------------------------------------ */
+    /*                    PRIVATE FUNCTIONS                   */
+    /* ------------------------------------------------------ */
+
+    function _updateReserves(uint256 balanceA, uint256 balanceB) private {
+        reserveA_ = balanceA;
+        reserveB_ = balanceB;
     }
 }
